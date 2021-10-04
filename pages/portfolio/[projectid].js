030b229b03071c216c89ed879c547c4ac5d3b321
Guillaume <Gui.lefort.17@gmail.com>
2021-10-04 12:15:06 +0200

    init and detail nested route in clients folder

--- pages/portfolio/[projectid].js
+++ pages/portfolio/[projectid].js
@@ -2,7 +2,17 @@
 // [] = placeholder
 // here's gonna be : http://localhost:3000/porttfolio/1 or anything beside of /1
 
+import { useRouter } from 'next/router';
+
 function PortfolioProjectPage() {
+  const router = useRouter();
+
+  // router.pathname is the path made by NEXTjs : portfolio/[projectid]
+  // router.query is made by user : /porttfolio/1 or anything beside of /1
+
+  console.log(router.pathname);
+  console.log(router.query);
+
   return (
     <div>
       <h1>The unique portfolio page.</h1>
