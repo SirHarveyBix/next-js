8959151ab5f5b5357ae9ab74b65517010a27a4d2
Guillaume <Gui.lefort.17@gmail.com>
2021-10-04 14:41:08 +0200

    add comment on clients/[id]/index.js

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -2,17 +2,19 @@ import { useRouter } from 'next/router';
 
 function ClientProjectsPage() {
   const router = useRouter();
+
   function loadProjectHandler() {
+    // set up a new url with router : pathname is the argument & query is what will be sent to the url to get on that specific page
     router.push({
       pathname: '/clients/[id]/[clientprojectid]',
-      query: { id: 'max', clientprojectid: 'projecta' },
+      query: { id: 'max', clientprojectid: 'maxproject' },
     });
   }
 
   return (
     <div>
       <h1>The projects of a given client</h1>
-      <button onClick={loadProjectHandler}>Load a Project</button>
+      <button onClick={loadProjectHandler}>Load max's Project</button>
     </div>
   );
 }
