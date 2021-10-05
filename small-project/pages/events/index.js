5a4660693e82e46153cfaea56527dc1b22892bb9
Guillaume <Gui.lefort.17@gmail.com>
2021-10-05 15:08:10 +0200

    add search result, some style for errors

--- small-project/pages/events/index.js
+++ small-project/pages/events/index.js
@@ -1,12 +1,20 @@
 import { EventList } from '../../components/events/EventList';
+import { useRouter } from 'next/router';
 import { getAllEvents } from '../../data';
 import EventsSearch from '../../components/events/EventsSearch';
+
 function EventsPage() {
   const event = getAllEvents();
+  const router = useRouter();
+
+  const findEventsHandler = (year, month) => {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  };
 
   return (
     <>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={event} />
     </>
   );
