3d558edb4a1029e3519d5b88e7db41b47e6e87de
Guillaume <Gui.lefort.17@gmail.com>
2022-02-08 17:32:02 +0100

    import Image = image optimization.

--- small-project/components/events/EventItem.jsx
+++ small-project/components/events/EventItem.jsx
@@ -3,6 +3,7 @@ import classes from '../../styles/EventItem.module.css';
 import DateIcon from '../icons/date-icon';
 import AddressIcon from '../icons/address-icon';
 import ArrowRightIcon from '../icons/arrow-right-icon';
+import Image from 'next/image';
 
 export const EventItem = (props) => {
   const { title, image, date, location, id } = props;
@@ -17,7 +18,7 @@ export const EventItem = (props) => {
 
   return (
     <li className={classes.item}>
-      <img src={`/${image}`} alt={title} />
+      <Image src={`/${image}`} alt={title} width={250} height={160} />
       <div className={classes.content}>
         <div className={classes.summary}>
           <h2>{title}</h2>
