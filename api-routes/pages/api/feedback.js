1f135e4d6d7aef697a31f030eb52735fdf2ba904
Guillaume <Gui.lefort.17@gmail.com>
2022-02-10 14:55:39 +0100

    refacto & add get request

--- api-routes/pages/api/feedback.js
+++ api-routes/pages/api/feedback.js
@@ -3,23 +3,19 @@ import { v4 as uuidv4 } from 'uuid';
 import fs from 'fs';
 import path from 'path';
 
+const builFeedbackPath = () => {
+  return path.join(process.cwd(), 'data', 'feedback.json');
+};
+
+const extractFeedback = (filePath) => {
+  const fileData = fs.readFileSync(filePath);
+  const data = JSON.parse(fileData);
+  return data;
+};
+
 async function handler(request, response) {
   if (request.method === 'POST') {
-    // stored in ../../data/feedback.json :
-    // create Path
-    const filePath = path.join(process.cwd(), 'data', 'feedback.json');
-    // verify if file exists
-    const isFileExists = fs.existsSync(filePath);
-    // create file if dosen't exists
-    if (!isFileExists) {
-      fs.appendFile(filePath, '[]', (err) => {
-        if (err) throw err;
-      });
-    }
-    //read feedback.json
-    const fileData = fs.readFileSync(filePath);
-    // parse full file
-    const data = JSON.parse(fileData);
+    const filePath = builFeedbackPath();
 
     const email = request.body.email;
     const feedbackText = request.body.text;
@@ -29,14 +25,24 @@ async function handler(request, response) {
       text: feedbackText,
     };
 
-    // push newFeedback into it
-    await data.push(newFeedback);
-    // write down in the file
+    // create file if dosen't exists
+    const isFileExists = fs.existsSync(filePath);
+    if (!isFileExists) {
+      fs.appendFile(filePath, '[]', (error) => {
+        if (error) throw error;
+      });
+    }
+
+    const data = extractFeedback(filePath);
+    data.push(newFeedback);
     fs.writeFileSync(filePath, JSON.stringify(data));
-    // send response : that worked, then shows what data has been wrriten
 
     response.status(201).json({ message: 'Succed !', feedback: newFeedback });
-  } else response.status(200).json({ message: 'Hello Word' });
+  } else {
+    const filePath = builFeedbackPath();
+    const data = extractFeedback(filePath);
+    response.status(200).json({ feedback: data });
+  }
 }
 
 export default handler;
