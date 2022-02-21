7692bd3a206f21d7f9c0192d2b873bb104397f93
Guillaume <Gui.lefort.17@gmail.com>
2022-02-21 11:45:26 +0100

    remove useless db-util.js

--- blog/lib/db-utils.js
+++
@@ -1,7 +0,0 @@
-import { MongoClient } from 'mongodb';
-import 'dotenv/config';
-
-export const connectDatabase = async () => {
-  const client = await MongoClient.connect(process.env.DB);
-  return client;
-};
