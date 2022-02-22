c3e1e1bb4e7b3e3031586b0351900af1515f7d56
Guillaume <Gui.lefort.17@gmail.com>
2022-02-22 12:06:35 +0100

    fix build

--- blog/pages/index.js
+++ blog/pages/index.js
@@ -1,14 +1,15 @@
-import FeaturedPosts from '../components/HomePage/FeaturedPosts';
-import Hero from '../components/homePage/Hero';
+import FeaturedPosts from '/components/HomePage/FeaturedPosts';
+import Hero from '/components/homePage/Hero';
 import { getFeaturedPosts } from '../lib/posts-utils';
 import Head from 'next/head';
+
 function HomePage(props) {
   const { posts } = props;
 
   return (
     <>
       <Head>
-        <title>le Blog tech de Guillaume</title>
+        <title>Le Blog tech de Guillaume</title>
         <meta name="description" content="programation et developpement" />
       </Head>
       <Hero />
